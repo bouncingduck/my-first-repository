e30e45e7aab9260ee512f8f86cb87f6bd02e33dd
Danny Hausmann <bouncingduck@gmail.com>
2020-09-07 14:30:38 -0400

    added name.rb

---
+++ name.rb
@@ -0,0 +1,9 @@
+puts "What's your name?"
+greeting = gets.chomp
+puts "Hello you fuckwad.  I mean, " + greeting
+
+puts "What's your last name?"
+last_name = gets.chomp
+10.times do |n|
+  puts greeting+" "+last_name
+end
